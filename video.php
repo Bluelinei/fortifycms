f57f78bb492f21419ef682f300303ed8bc43c963
Zach Watts <zach@zachwattscreates.com>
2016-10-26 15:12:29 -0400

    updated footer and header files, combined logout button into footer

--- video.php
+++ video.php
@@ -1,7 +1,14 @@
 <?php require 'header.php'; ?>
 <!--START Content-pane-->
-    <div class="content-pane" id="page-body">
+  <div class="content-pane" id="page-body">
 
-<div class="ten-per-wide right" style="height: 100%; background: #222;"></div>
+
+    <div class="left ninty-per-wide fourty-padding" style="background: #fdfdfd; height: 100%;">
+      <video style="width: 100%;" controls>
+        <source src="video/test-video.mp4" type="video/mp4">
+      </video>
+    </div>
+    <div class="ten-per-wide right" style="height: 100%; background: #222;"></div>
+    <div class="clear"></div>
 
 <?php require 'footer.php'; ?>
