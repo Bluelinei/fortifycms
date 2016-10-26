c63b67df78f93d0089ecab42d8fbdb24449a957d
Tyler <tyler@bluelinesolutions.org>
2016-10-26 12:21:39 -0400

    Finished case posting and updating.

--- framework/functions.php
+++ framework/functions.php
@@ -1,35 +1,52 @@
 <?php
 
-$hostname = '192.168.1.24';
-$port = '3306';
-$username='zhwatts';
-$pass='';
-$database = 'fortify';
+$hostname=	'localhost';
+$port=		'3306';
+$username=	'root';
+$pass=		'';
+$database=	'fortify';
 
 error_reporting(E_ALL);
 
-function getData()
+function getError($e) {die("CASEPOST: ".$e->getMessage());}
+
+try{
+	$conn = new PDO("mysql:host=$hostname; port=$port; dbname=$database; charset=UTF8;", $username, $pass);
+} catch(PDOException $e) {getError($e);}
+
+function getData($conn)
 {
 
 }
 
-function checkUID()
+function getUID($conn)
 {
-	try{
-		$hostname = '192.168.1.24';
-		$port = '3306';
-		$username='zhwatts';
-		$pass='';
-		$database = 'fortify';
-		$conn = new PDO("mysql:host=$hostname; port=$port; dbname=$database; charset=UTF8;", $username, $pass);
-		$ifexist = "SELECT EXISTS(SELECT 1 FROM ". $_POST['table'] ." WHERE uid=".$_POST['uid'];
-		$stmt = $conn->prepare($ifexist);
-		$stmt->execute();
-		$result = $stmt->fetch();
-		echo $result;
-	} catch(PDOException $e) {
-		echo "ERROR: ".$e->getMessage();
+	$hostname=	'localhost';
+	$port=		'3306';
+	$username=	'root';
+	$pass=		'';
+	$database=	'fortify';
+	//GENERATE RANDOM NUMBER
+	$hex = array("0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F");
+	$genuid;
+	$status = true;
+	while($status)
+	{
+		$status=false;
+		$genuid = "";
+		while(strlen($genuid)<16)
+		{
+			$genuid .= $hex[rand(0,15)];
+		}
+
+		try {
+			$ifexist = "SELECT EXISTS(SELECT 1 FROM ? WHERE uid=?";
+			$stmt = $conn->prepare($ifexist);
+			$stmt->execute(array($_POST['table'], $genuid));
+			if($stmt->fetch()) $status=true;
+		} catch(PDOException $e) {getError($e);}
 	}
+	echo $genuid;
 }
 
 if(isset($_POST['function'])&&isset($_POST['table']))
@@ -37,15 +54,17 @@ if(isset($_POST['function'])&&isset($_POST['table']))
 	switch($_POST['function'])
 	{
 		case 'get':
-			getData();
+			getData($conn);
 			break;
-		case 'checkuid':
-			checkUID();
+		case 'getuid':
+			getUID($conn);
 			break;
 		default:
 			echo "Undefined function: ".$_POST['function'];
 			break;
 	}
+} else {
+	echo "Unset function or table rules.";
 }
 
 ?>
