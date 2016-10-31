e059d163b58bfad06b11f1a6f3725f2fe30c3f0d
Zach Watts <zach@zachwattscreates.com>
2016-10-31 14:22:39 -0400

    Login Screen

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Please Login</title>
+    <link href="css/style.css" rel="stylesheet"/>
+  </head>
+  <body style= "background: linear-gradient(#bfbfbf, #fff);">
+
+      <div class="vertical-middle" style="width:500px;margin: 0px auto;">
+        <img src="img/Fortify-Logo-Brushed.png" style="width: 100%;" />
+        <input type="text" placeholder="User Name" class="left"/>
+        <input type="text" placeholder="Password" class="right"/>
+        <div class="clear"></div>
+        <input type="submit" value="Login">
+        <div class="msg">
+          Please Login First
+        </div>
+      </div>
+
+  </body>
+</html>
