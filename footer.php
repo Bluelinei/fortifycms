d6b89831649031dd4f874ab331a04aa217115780
Zach Watts <zach@zachwattscreates.com>
2016-11-02 12:06:13 -0400

    added notification styling in the notification center

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <!--START footer-->
   <footer>
 
-    <a href="" class="logout-button"><p class="vertical-middle">Log Out</p></a>
+    <a href="" class="logout-button"><p class="vertical-middle"><i class="fa fa-lock" aria-hidden="true"></i> Log Out</p></a>
 
 
     <div class="twenty-per-wide notification-button point-cursor">
--- notification-bar.php
+++ notification-bar.php
@@ -8,13 +8,80 @@
          <i class="fa fa-chevron-circle-<?php if(1 == 1){echo 'down';} else {echo 'up';} ?>" aria-hidden="true" style="position: absolute; right: 0; z-index: 1100;color: #fff;margin-top: 5px;margin-right: 30px;"></i>
   </div>
 
-  <div style="width: 100%; height: 60px; background: #509ace;margin: 1px 0; color: #fff;">
+  <div style="width: 100%; height: 60px; background: #509ace;margin: 1px 0; color: #fff;" class="point-cursor">
     <div class="quick-notify-close left">
         <div class="vertical-middle"><i class="fa fa-times-circle" aria-hidden="true"></i></div>
     </div>
     <div class="quick-notify-content left">
-      <div class="vertical-middle">You have 12 cases to follow-up on</div>
+      <div class="vertical-middle">You have 3 cases to follow-up on</div>
+    </div>
+    <div class="clear"></div>
+    <div style="background: #509ace;">
+      <ol class="notify-cases">
+        <li>
+          <div class="thirty-per-wide text-center left" style="background: rgba(000,000,000,.1);padding: 15px 0;">16SL0000098</div>
+          <div class="seventy-per-wide text-center left" style="padding: 15px 0;">Roadside DUI</div>
+          <div class="clear"></div>
+        </li>
+        <li>
+          <div class="thirty-per-wide text-center left li-inner" style="background: rgba(000,000,000,.1); padding: 15px 0;">16SL0000100</div>
+          <div class="seventy-per-wide text-center left" style="padding: 15px 0;">Grocery Domestic</div>
+          <div class="clear"></div>
+        </li>
+        <li>
+          <div class="thirty-per-wide text-center left" style="background: rgba(000,000,000,.1); padding: 15px 0;">16SL0000103</div>
+          <div class="seventy-per-wide text-center left" style="padding: 15px 0;">Traffic Stop</div>
+          <div class="clear"></div>
+        </li>
+      </ol>
     </div>
   </div>
 
+<div class="clear"></div>
+
+  <div style="width: 100%; height: 60px; background: #ff4444;margin: 3px 0; color: #fff;" class="point-cursor">
+    <div class="quick-notify-close left">
+        <div class="vertical-middle"><i class="fa fa-times-circle" aria-hidden="true"></i></div>
+    </div>
+    <div class="quick-notify-content left">
+      <div class="vertical-middle">New message from Cheif Skywalker </div>
+    </div>
+    <div class="clear"></div>
+  </div>
+
+  <div style="width: 100%; height: 60px; background: #ff4444;margin: 3px 0; color: #fff;" class="point-cursor">
+    <div class="quick-notify-close left">
+        <div class="vertical-middle"><i class="fa fa-times-circle" aria-hidden="true"></i></div>
+    </div>
+    <div class="quick-notify-content left">
+      <div class="vertical-middle">New message from Sergeant Solo </div>
+    </div>
+    <div class="clear"></div>
+  </div>
+
 </div>
+
+<style>
+
+.notify-cases{
+  display: none;
+}
+
+  .notify-cases li{
+    background: rgba(000,000,000,0.2);
+    border-bottom: 1px solid #333;
+    cursor: pointer;
+    font-size: 10px;
+    text-align: left;
+    transition: all .08s;
+  }
+
+
+  .notify-cases li:nth-child(even){
+    background: rgba(000,000,000,0.4);
+  }
+
+  .notify-cases li:hover{
+    background: rgba(255,255,255,0.1);
+  }
+</style>
